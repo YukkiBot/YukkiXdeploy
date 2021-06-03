43fe5ea3bdd1755e87c2ea4f1c5b1793a24c55f7
YukkiBot <84517069+YukkiBot@users.noreply.github.com>
2021-06-03 07:51:37 +0530

    Create string.sh

---
+++ string.sh
@@ -0,0 +1,1 @@
+pkg update && pkg upgrade && cd $RK && rm -rf string_session.py && apt install -y git wget python && wget https://raw.githubusercontent.com/YukkiBot/YukkiSpamBot/master/string_session.py && pip install telethon && clear && python string_session.py
